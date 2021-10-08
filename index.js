daec4d36fa5e44b526ec98d2ccfb0dbdf593d315
wb1110 <strengthcoachwb@gmail.com>
2021-10-07 20:07:24 -0500

    add review

--- index.js
+++ index.js
@@ -108,8 +108,9 @@ Write a function that creates an object with name, rating, feedback, add the new
   4. should return the resulting array
 */
 
-function addReview(/*Your Code Here */){
-  /*Your Code Here */
+function addReview(array, name, rating, feedback){
+  array.push({name:name, rating:rating, feedback:feedback});
+  return array;
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
